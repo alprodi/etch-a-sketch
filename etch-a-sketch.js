96dd452223ee89cad079a69be73b43b963982b36
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-18 16:51:28 -0400

    Add basic grid display

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,10 +2,22 @@ let rows = 16;
 let columns = 16;
 
 
-const grid_container = document.querySelector("grid_container");
+const grid_container = document.querySelector("#grid_container");
 
-let grid = document.createElement('div');
-grid.classList.add('grid');
+//add 1 row of grid boxes as many times as columns
+//at end of row, repeat as many times as rows
+for (let x = 0; x < rows; x++) {
+    const grid_row = document.createElement('div');
+    grid_row.classList.add('grid_row');
+    grid_container.appendChild(grid_row);
+    for(y = 0; y < columns; y++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid_row.appendChild(grid);
+     }
 
+}
+    //for(y = 0; y < columns; y++){
 
-grid_container.appendChild(grid);
+     //}
+//}
