6ce1e0b746a432506a073771610049b9e66a17ed
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-18 16:07:20 -0400

    Prepare grid style

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+let rows = 16;
+let columns = 16;
+
+
+const grid_container = document.querySelector("grid_container");
+
+let grid = document.createElement('div');
+grid.classList.add('grid');
+
+
+grid_container.appendChild(grid);
