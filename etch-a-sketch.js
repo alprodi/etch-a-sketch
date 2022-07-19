c290cefed48d8059e5552ca0a79928134661a671
alprodi <106893267+alprodi@users.noreply.github.com>
2022-07-18 20:05:11 -0400

    Tweak input area appearance

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,10 @@
 let rows = 16;
 let columns = 16;
 
-const grid_container = document.querySelector('#grid_container');
 
 //add 1 row of grid boxes as many times as columns
 //at end of row, repeat as many times as rows
+const grid_container = document.querySelector('#grid_container');
 for (let x = 0; x < rows; x++) {
     const grid_row = document.createElement('div');
     grid_row.classList.add('grid_row');
@@ -20,10 +20,23 @@ for (let x = 0; x < rows; x++) {
 }
 
 //reset grid backgrounds
-const reset_button = document.querySelector("#reset_button");
+const reset_button = document.querySelector('#reset_button');
 reset_button.onclick = function(){
     const grid_reset = document.querySelectorAll('.grid_black');
     grid_reset.forEach(function(to_remove){
         to_remove.classList.remove('grid_black');
     });
 }
+
+//row and column input forms
+const rows_input = document.querySelector('#rows_input');
+const rows_input_form = document.createElement('form');
+rows_input_form.value = '1';
+rows_input_form.type = text;
+rows_input_form.setAttribute('type', text)
+
+
+rows_input.appendChild(rows_input_form);
+
+
+const columns_input = document.querySelector('#columns_input');
